e76f76fc0688dee1fb56e638dd541ab237e6ce1a
wocheon <ciw0707@naver.com>
2023-09-13 07:42:12 +0000

    202309130742

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python
-print("hello world")
+
+var="Hello, World"
+
+print var
+print var * 2
+
+result= "hello" in var
+print result
+
+print var[4]
+print var[7:]
+
+print var.split()
+print var.split(',')
+
+print var.find(',')
+print var.replace(',', '')
+
+print var.upper()
+print var.lower()
+
+list=list(var)
+print list
+
+tup=tuple(var)
+print tup
