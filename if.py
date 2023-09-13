c828e45d580d7437dce938cb4c89e36277c958bc
wocheon <ciw0707@naver.com>
2023-09-13 08:09:39 +0000

    202309130809

--- if.py
+++ if.py
@@ -7,8 +7,10 @@ while a<=30:
 	div_mod=divmod(a, 5)
 	if div==0:
 		print( str(a) + ": true " +  str(div_mod) )
+	elif div==1:
+		print( str(a) + ": check " +  str(div_mod) )
 	else:
 		print( str(a) + ": false " +  str(div_mod) )
 	a = a + 1
 
-print(done)
+print("done")
