6e20e30603c99be0c37f9c93c96b2b6b9812c0ce
wocheon <ciw0707@naver.com>
2023-09-14 04:59:05 +0000

    202309140459

--- subprocess/subp_stdin_pipe.py
+++ subprocess/subp_stdin_pipe.py
@@ -13,8 +13,6 @@ proc.stdin.flush()
 print(proc.stdout.readline())
 
 sh_proc = subprocess.Popen("sh read_var.sh", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-#sh_proc.stdin.write('Stdin_Input_Test : bash\n')
-#sh_proc.stdin.flush()
 sh_stdout, sh_stderr=sh_proc.communicate(input="Stdin_Input_test : /bin/bash\n")
 
 print(sh_stdout)
